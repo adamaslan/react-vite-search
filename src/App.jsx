623c55576da907b9eaaff5846110d198b04fcc88
Adam <chillcoders@gmail.com>
2022-03-29 19:41:48 -0400

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,63 @@
-import react from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import * as React from "react";
 
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle("React")}</h1>
+      <h1>My Hacker Stories</h1>
 
+      <Search />
+
+      <hr />
+
+      <List />
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
